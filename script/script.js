b9c4466e3ed5fb6a5cd24b92c3058fd26e8eb8b6
Arvind S Panicker <arvindspanicker@gmail.com>
2018-03-06 19:41:36 +0530

    Modified About Section

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
 function AboutMe(){
 	document.getElementById('id01').style.display='block';
+	document.getElementById("myname").style.display='none';
+	document.getElementById("mydesg").style.display='none';
 }
