a5a01106967e8b7a8e1aa1a0dad21b4a6026751d
Arvind S Panicker <arvindspanicker@gmail.com>
2018-03-09 03:03:00 +0530

    Added Contact Modal and slight changes to About Modal

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
-function AboutMe(){
+function AboutModal(){
 	document.getElementById('id01').style.display='block';
+	document.getElementById("myname").style.display='none';
+	document.getElementById("mydesg").style.display='none';
+}
+
+function ContactModal(){
+	document.getElementById('id02').style.display='block';
 	document.getElementById("myname").style.display='none';
 	document.getElementById("mydesg").style.display='none';
 }
